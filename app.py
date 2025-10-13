9c0f14f59f5b98196575632969f8feba58f47cd4
ediesonmalabag-source <ediesonmalabag@gmail.com>
2025-10-13 11:58:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,6 @@ def chatbot_response(user_message: str) -> str:
         return """<h4 style='color:#003366; font-weight:bold;'>📞 TESDA-BIT Contact Information</h4>
         🌐 <b>Facebook Page:</b> <a href='https://www.facebook.com/profile.php?id=61561653118631' target='_blank' style='color:#003366; font-weight:italic;'>Bangui Institute of Technology TESDA</a><br>
         💬 <b>Messenger Chat:</b> <a href='https://www.facebook.com/messages/t/61561653118631' target='_blank'>Send a message via Messenger</a><br>
-        #💬 <b>Messenger Chat:</b> <a href='https://m.me/BanguiInstituteofTechnologyTESDA' target='_blank' style='color:#003366; font-weight:italic;'>Message us on Messenger</a><br>
         📱 <b>Cellphone No.:</b> 0908-860-0955<br>
         📧 <b>Email:</b> <a href='mailto:bit@tesda.gov.ph'>bit@tesda.gov.ph</a><br>
         📍 <b>Office Address:</b> TESDA-Bangui Institute of Technology, Brgy. Manayon, Bangui, Ilocos Norte<br><br>
