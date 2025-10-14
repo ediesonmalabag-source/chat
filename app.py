a923a4e988e82371034e7a7cb1cfb381e954e653
ediesonmalabag-source <ediesonmalabag@gmail.com>
2025-10-14 12:42:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -380,8 +380,13 @@ for role, msg in st.session_state.messages:
             f"🧑 <b>{role}:</b> {msg}</div>",
             unsafe_allow_html=True,
         )
-    else:
-        st.markdown(msg, unsafe_allow_html=True)
+  else:
+        st.markdown(
+            f"<div style='background-color:#e6f2ff; padding:10px 15px; border-radius:10px; margin-top:-6px; margin-bottom:10px; color:#003366;'>"
+            f"{msg}"
+            f"</div>",
+            unsafe_allow_html=True,
+        
 
 
 # --------------------------
