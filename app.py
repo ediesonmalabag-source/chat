602a5dc7f3662c09a1a8819922bac36feb6d4ab3
ediesonmalabag-source <ediesonmalabag@gmail.com>
2025-10-15 22:57:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -566,7 +566,7 @@ if st.session_state.get("show_enrolment_form") == "form":
                     flattened_pdf = PdfReader(tmp_out_path)
                     for page in flattened_pdf.pages:
                         if PdfName("/Annots") in page:
-                        del page[PdfName("/Annots")]
+                            del page[PdfName("/Annots")]
                     PdfWriter().write(tmp_out_path, flattened_pdf)
 
                     if isinstance(ok_err, tuple):
