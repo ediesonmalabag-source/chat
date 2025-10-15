7abd279462526112c7f9cc110de82aebdc7f5569
ediesonmalabag-source <ediesonmalabag@gmail.com>
2025-10-15 17:02:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ is_mobile = screen_width < 768
 is_messenger = "FBAN" in user_agent or "Messenger" in user_agent
 
 if "show_mobile_warning" not in st.session_state:
-    if user_agent and "FBAN" in user_agent or "Messenger" in user_agent:
+    if user_agent and ("FBAN" in user_agent or "Messenger" in user_agent):
         st.session_state.show_mobile_warning = screen_width < 768
     else:
         st.session_state.show_mobile_warning = False
